4942ccaf3c715ffbe8b75887493b23236fb38268
nnanna johnso okoro <okoronnanna337@gmail.com>
2025-09-03 18:22:48 +0100

    feat: Updated server and vercel

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-{
-  /*const express = require("express");
+const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/Auth");
 const productRoutes = require("./routes/product");
@@ -41,46 +40,3 @@ app.use("/api/products", productRoutes);
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
-*/
-}
-
-const express = require("express");
-const mongoose = require("mongoose");
-const authRoutes = require("./routes/Auth");
-const productRoutes = require("./routes/product");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("DB connection successful");
-  } catch (error) {
-    console.error("DB connection failed:", error.message);
-  }
-};
-
-connectDB();
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: " Hello! Your backend server is running!",
-    data: {
-      name: "e-commerce-backend data",
-      class: "feb 2025",
-      efficiency: "beginner",
-    },
-  });
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-
-// ❌ Remove app.listen()
-// ✅ Export the app instead
-module.exports = app;
