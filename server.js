d2b7a3bc9172dbd9c392fac1e67493928a74f19d
nnanna johnso okoro <okoronnanna337@gmail.com>
2025-09-05 22:48:49 +0100

    updated server vand vercel

--- server.js
+++ server.js
@@ -6,26 +6,28 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-const connectDB = async () => {
+let isDbReady = false;
+async function connectDB() {
+  if (isDbReady || mongoose.connection.readyState >= 1) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  isDbReady = true;
+  console.log("DB connection successful");
+}
+app.use(async (_req, _res, next) => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("DB connection successful");
-  } catch (error) {
-    console.error("DB connection failed:", error.message);
+    await connectDB();
+  } catch (e) {
+    console.error("DB error:", e);
   }
-};
+  next();
+});
 
-connectDB();
-
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.status(200).json({
-    message: " Hello! Your backend server is running!",
+    message: "Hello! Your backend server is running!",
     data: {
       name: "e-commerce-backend data",
       class: "feb 2025",
@@ -37,6 +39,4 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+module.exports = (req, res) => app(req, res);
